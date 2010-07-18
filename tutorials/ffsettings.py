9967aefb0a8092a1137f7257daa60596b0b32d3f
Y Devesh <dev344@gmail.com>
2010-07-18 16:43:43 +0530

    Made some changes in ffsettings.py and added a few functions in observer.py

--- tutorials/ffsettings.py
+++ tutorials/ffsettings.py
@@ -5,49 +5,71 @@
 # For Application : Daskalos
 # Applications used here : Dogtail-0.7.0
 # Other info : This was written for the application Daskalos to be included in the distro 'Shaastra Distro'
+# Contact : shaastra-distro-2010@googlegroups.com
 # ########################################################################################################################
 
-    """
-    	What the next few lines do : This code here uses dogtail to open the network settings in firefox.
-    	Small explanation for the code : 
-    """
+"""
+   		What the next few lines do : This code here uses dogtail to open the network settings in firefox.
+   		Small explanation for the code : Functions like click, focus.frame, etc are all part of dogtail's procedural module
+   										 which is imported in the observer module.
+"""
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from dogtail.procedural import *
-import dogtail.rawinput
 
-trylimit=0
+def mainProgram():
+	""" In this function things are hardcoded a bit.Also dogtail modules are imported inside.
+	"""
+	from observer import *                             #if i am using * then i should mention what all come under * later
+	import dogtail.rawinput
 
-focus.application('gnome-panel')
-#focus.frame('Top Expanded Edge Panel')             #This line has to be removed completely later
-click('Applications', roleName='menu')
-click('Internet', roleName='menu')
-click('Firefox Web Browser', roleName='menu item')
-while((not focus.application('Firefox'))and(trylimit<30)):
-    trylimit += trylimit
+	observer = Observer()                              
 
-children_of_firefox = FocusApplication.node._getChildren()
-framename = children_of_firefox[0]._get_name()
-focus.frame(framename)
-click('Edit', roleName='menu')
-click('Preferences', roleName='menu item')
-dogtail.rawinput.doTypingDelay()                #These delays have been included because  
-dogtail.rawinput.doTypingDelay()                #children_of_firefox is being found out 
-dogtail.rawinput.doTypingDelay()                #a bit earlier and hence there could be 
-dogtail.rawinput.doTypingDelay()                #a index out of range without this.
-children_of_firefox = FocusApplication.node._getChildren()
-preference_window = children_of_firefox[1]._get_name()
-focus.frame(preference_window)
-click('Advanced')
-click('Network')
-scroll_pane = children_of_firefox[1]._getChildren()
-panel = scroll_pane[7]._getChildren()      #scroll_pane[7] is Advanced options
-scroll_pane2 = panel[0]._getChildren()    
-panel2 = scroll_pane2[2]._getChildren()
-next_child = panel2[0]._getChildren()       #panel2 is Connection option in Network option in Advanced options
-settings = next_child[2]
-x,y = settings.position
-dogtail.rawinput.click(x, y, 1)
-children_of_firefox = FocusApplication.node._getChildren()
-cnctn_sttngs_win = children_of_firefox[2]._get_name()
-focus.frame(cnctn_sttngs_win)
+
+	observer.openWindow('Applications', 'Internet', 'Firefox Web Browser')
+	if(not observer.isFocussed('Firefox')):                         #if i can somehow avaid such statements i can make this code more general i think
+		raise NameError("Could not focus Firefox")       
+     
+	children_of_firefox = FocusApplication.node.children
+	newWindowNum = len(children_of_firefox) - 1
+	frameName = children_of_firefox[newWindowNum].name
+	if(not observer.frameFocussed(frameName)): 
+		raise NameError("Could not focus frame " + frameName)
+	
+	click('Edit', roleName='menu')
+	click('Preferences', roleName='menu item')
+	dogtail.rawinput.doTypingDelay()                #These delays have been included because  
+	dogtail.rawinput.doTypingDelay()                #children_of_firefox is being found out 
+	dogtail.rawinput.doTypingDelay()                #a bit earlier and hence there could be 
+	dogtail.rawinput.doTypingDelay()                #a index out of range without this.
+	children_of_firefox = FocusApplication.node.children
+	prefWindowNum = len(children_of_firefox) - 1
+	if(prefWindowNum <= newWindowNum ): 
+		raise NameError("Preference window did not open")        
+	prefWindow = children_of_firefox[prefWindowNum].name
+	if(not(prefWindow == "Firefox Preferences")):                     #double-check.Can also be removed to make the code more general
+		raise NameError("Preference Window did not open")
+	if(not observer.frameFocussed(prefWindow)): 
+		raise NameError("Could not focus frame " + prefWindow)
+	
+	click('Advanced')
+	click('Network')
+	scroll_pane = children_of_firefox[prefWindowNum].children
+	panel = scroll_pane[7].children       #scroll_pane[7] is Advanced options
+	scroll_pane2 = panel[0].children    
+	panel2 = scroll_pane2[2].children
+	next_child = panel2[0].children       #panel2 is Connection option in Network option in Advanced options
+	settings = next_child[2]
+	x,y = settings.position
+	dogtail.rawinput.click(x, y, 1)
+	children_of_firefox = FocusApplication.node.children
+	cntnsWindowNum = len(children_of_firefox) - 1
+	cnctn_sttngs_win = children_of_firefox[cntnsWindowNum].name
+	if(not observer.frameFocussed(cnctn_sttngs_win)): 
+		raise NameError("Could not focus frame " + cnctn_sttngs_win)
+		
+def main():
+	import subprocess
+	mainProgram()
+	
+if __name__=='__main__' :
+    main()
--- tutorials/observer.py
+++ tutorials/observer.py
@@ -1,10 +1,59 @@
+# ########################################################################################################################
+# Date : 15th July 2010
+# Written by : Devesh Yamparala
+# Mentored by : Arun Chaganty
+# For Application : Daskalos
+# Applications used here : Dogtail-0.7.0
+# Other info : This was written for the application Daskalos to be included in the distro 'Shaastra Distro'
+# Contact : shaastra-distro-2010@googlegroups.com
+# ########################################################################################################################
+
+
+
 from dogtail.procedural import *
 
 class Observer():
 	def isFocussed(self, appName, trylimit = 30):
 		attemptNum = 0
 		while((not focus.application(appName))and(attemptNum<trylimit)):
-			attemptNum += attemptNum
-		if(attemptNum == trylimit): return False
+			attemptNum +=  1
+		if(attemptNum == trylimit): raise NameError ("Could not focus the application " + appName)
 		else: return True
+		
+	def frameFocussed(self, frameName, trylimit = 3):
+		print "focussing ", frameName                   #debug statement
+		attemptNum = 0
+		while((not focus.frame(frameName))and(attemptNum<trylimit)):
+			attemptNum += 1
+		if(attemptNum == trylimit): raise NameError ("Could not focus the frame " + frameName)
+		else: return True
+		
+	def searchApp(self):
+		"""Just an idea.Have to see if something comes out of it.
+		"""
+		# Synaptic = ('Sytem', 'Administration', 'Synaptic Package Manager')
+		# Software Sources = ('Sytem', 'Administration', 'Software Souces')
+		# Users and groups = ('Sytem', 'Administration', 'Users and Groups')
+		# Time and Date = ('Sytem', 'Administration', 'Time and Date')
+		# Startup Manger = ('Sytem', 'Administration', 'StartUp-Manager')
+		# Login Screen = ('Sytem', 'Administration', 'Login Screen')
+		# Network Tools = ('Sytem', 'Administration', 'Network Tools')
+		# Firefox Web Browser = ('Applications', 'Internet' ,'Firefox Web Browser')
+		# Gedit Text Editor = ('Applications', 'Accessories', 'gedit Text Editor')
+		# Gvim Text Editor = ('Applications', 'Accessories', 'Gvim Text Editor')
+		# Kate Text Editor = ('Applications', 'Accessories', 'Kate')
+		# Gnome Terminal = ('Applications', 'Accessories', 'Terminal')
+		pass
+		
+	def openWindow(self,*args):
+		"""This functions should open the window of applications which are there in gnome panel
+		"""
+		if(not self.isFocussed('gnome-panel')): 
+			raise NameError("Could not focus gnome-panel")
+
+		click(args[0], roleName='menu')
+		click(args[1], roleName='menu')
+		click(args[2], roleName='menu item')
+		
+		
 		
