7631192c6a57801b77004a1a3782f42c1642d9e7
Y Devesh <dev344@gmail.com>
2010-07-29 01:07:20 +0530

    Small change

--- daskalos/daskalos.py
+++ daskalos/daskalos.py
@@ -14,10 +14,10 @@ pathname = os.path.dirname(sys.argv[0])
 full_path = os.path.abspath(pathname)
 os.environ["DSK_HOME"] = full_path
 ui_path = os.path.join(full_path, 'src/ui/')
-if os.path.isfile(os.path.join(ui_path, "gladepreview.py")):
+if os.path.isfile(os.path.join(ui_path, "daskalosui.py")):
     sys.path.append(ui_path)
-    ui_file = __import__('gladepreview')
+    ui_file = __import__('daskalosui')
     ui_file.daskalosUI.main()
 else :
-    print 'gladepreview.py file does not exist.Check if you are in the right folder'
+    print 'daskalosui.py file does not exist.Check if you are in the right folder'
 os.environ.pop("DSK_HOME")
