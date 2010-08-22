840aedd6500c6f25c678e287af73926e1c69382d
Y Devesh <dev344@gmail.com>
2010-08-22 14:55:11 +0530

    removed a gmt file

--- daskalos/src/tutorials/startup_applications_gmt.py
+++
@@ -1,8 +0,0 @@
-class GmtTutorials:
-	
-	description = """Choose what applications to start when you login.\nTo go there follow 'System'-> 'Preferences'-> 'Startup Applications'
-				   """
-	
-	dictionary = {'Startup Applications' : [('System', 'Preferences', 'Startup Applications'), description]}
-
-gmt_tut = GmtTutorials()
